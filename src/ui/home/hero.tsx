165cabec2c2894e6bc250c5b32f116c2f19a7dff
zaheer-bashir <zaheerkpr3@gmail.com>
2024-12-13 02:27:13 +0500

    Feat: Create Hero Section

---
+++ src/ui/home/hero.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/button";
+
+const Hero = () => {
+  return (
+    <section className="flex justify-center items-center gap-10 max-w-[1400px] mx-auto py-8 min-h-[60vh]">
+      <div className="w-1/2">
+        <h1 className="text-5xl">
+          Elevate Your Brand with Luxurious Web Presence.
+        </h1>
+        <p className="mt-8 mb-6 max-w-[520px] text-lg">
+          Custom websites tailored to reflect the elegance and sophistication of
+          your brand.
+        </p>
+        <Button title="Get Started" />
+      </div>
+      <Image src="/hero.png" alt="Hero Image" height="450" width="600" />
+    </section>
+  );
+};
+
+export default Hero;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/ui/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/layout/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*s.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
